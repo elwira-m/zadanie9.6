0966cee168218e94d3ebf6d15e8927d6f7c35d96
elwira-m <36117564+elwira-m@users.noreply.github.com>
2018-02-26 22:16:42 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+//script.js
+
+var list = document.getElementById('list');
+var add = document.getElementById('addElem');
+var listItems = document.getElementsByTagName('li');
+
+add.addEventListener('click', function() {
+	var element = document.createElement('li');
+
+    element.innerHTML = 'item ' + listItems.length;
+	list.appendChild(element);	
+});
